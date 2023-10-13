34d14567da089bb68330261f0be448f9e1793d9f
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-13 15:57:43 +0800

    File Added with check safe algorithm

---
+++ NQueen.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i, j = row, col
+    while i >= 0 and j < len(board):
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+    return True
